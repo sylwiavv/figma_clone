b8af6543cc2a40a289657b4b1051b74b17f0afcb
sylwia <sylwiavv@gmail.com>
2024-06-17 12:32:22 +0200

    Open and cclose message input

--- app/components/Live.tsx
+++ app/components/Live.tsx
@@ -3,7 +3,7 @@
 import {useOthers} from "@liveblocks/react/suspense";
 import {LiveCursors} from "@/app/components/cursror/LiveCursors";
 import {useMyPresence} from "@/app/liveblocks.config";
-import React, {useCallback, useState} from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import {CursorChat} from "@/app/components/cursror/CursorChat";
 import {CursorMode} from "@/types/type";
 
@@ -42,6 +42,37 @@ export const Live = () => {
         updateMyPresence({cursor: {x, y}})
     }, [])
 
+    useEffect(() => {
+        const onKeyUp = (e: React.KeyboardEvent) => {
+            if (e.key === "/") {
+                setCursorState({
+                    mode: CursorMode.Chat,
+                    previousMessage: null,
+                    message: ""
+                })
+            } else if (
+                e.key === "Escape"
+            ) {
+                setCursorState({mode: CursorMode.Hidden})
+                updateMyPresence({message: ""})
+            }
+        }
+
+        const onKeyDown = (e: React.KeyboardEvent) => {
+            if (e.key === "/") {
+                e.preventDefault()
+            }
+        }
+
+        window.addEventListener("keyup", onKeyUp)
+        window.addEventListener("keydown", onKeyDown)
+
+        return () => {
+            window.removeEventListener("keyup", onKeyUp)
+            window.removeEventListener("keydown", onKeyDown)
+        }
+    }, [updateMyPresence])
+
     return (
         <div onPointerMove={handlePointerMove}
              onPointerDown={handlePointerDown}
@@ -49,9 +80,10 @@ export const Live = () => {
              className="border-2 border-green-500 h-[100vh] w-full flex justify-center items-center text-center">
             <h1 className="text-2xl text-white">Liveblock fimga clone</h1>
 
-            {cursor && <CursorChat cursor={cursor} cursorState={cursorState} setCursorState={setCursorState} updateMyPresence={updateMyPresence} />}
+            {cursor && <CursorChat cursor={cursor} cursorState={cursorState} setCursorState={setCursorState}
+                                   updateMyPresence={updateMyPresence}/>}
 
-            <LiveCursors others={others} />
+            <LiveCursors others={others}/>
 
         </div>
     );
