428e4900f755b425a43b2315f0a24307202a59b0
sylwia <sylwiavv@gmail.com>
2024-06-18 10:18:47 +0200

    add NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_KEY

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 const nextConfig = {
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
+    env: {
+        PUBLIC_API_KEY: process.env.NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_KEY,
+    },
 };
 
 export default nextConfig;
