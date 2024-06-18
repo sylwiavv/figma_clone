0ecedd29b13faf4529639237ff7fd4cc6cffeee6
sylwia <sylwiavv@gmail.com>
2024-06-18 10:32:27 +0200

    update

--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -4,6 +4,7 @@ import { createRoomContext } from "@liveblocks/react";
 const client = createClient({
   throttle: 100,
   publicApiKey: process.env.NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_KEY!,
+  authEndpoint: "/api/liveblocks-auth",
 });
 
 // Presence represents the properties that will exist on every User in the Room
