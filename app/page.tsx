8176ac221f54c50f4ff44de0aeb9e2fdd478beed
sylwia <sylwiavv@gmail.com>
2024-06-22 12:29:48 +0200

    Add image

--- app/page.tsx
+++ app/page.tsx
@@ -8,21 +8,27 @@ import {useEffect, useRef, useState} from "react";
 import {fabric} from "fabric";
 import {
     handleCanvaseMouseMove,
-    handleCanvasMouseDown, handleCanvasMouseUp, handleCanvasObjectModified,
+    handleCanvasMouseDown, handleCanvasMouseUp, handleCanvasObjectModified, handleCanvasObjectMoving, handlePathCreated,
     handleResize,
     initializeFabric, renderCanvas,
 } from "@/lib/canvas";
 import {ActiveElement} from "@/types/type";
-import {useMutation, useStorage} from "@/liveblocks.config";
+import {useMutation, useRedo, useStorage, useUndo} from "@/liveblocks.config";
 import {defaultNavElement} from "@/constants";
+import {handleDelete, handleKeyDown} from "@/lib/key-events";
+import {handleImageUpload} from "@/lib/shapes";
 
 export default function Page() {
+    const undo = useUndo()
+    const redo = useRedo()
+
     const canvasRef = useRef<HTMLCanvasElement | null>(null)
     const fabricRef = useRef<fabric.Canvas | null>(null)
     const isDrawing = useRef(false)
     const shapeRef = useRef<fabric.Object | null>(null)
     const selectedShapeRef = useRef<string | null>(null)
     const activeObjectRef = useRef<fabric.Object | null>(null)
+    const imageInputRef = useRef<HTMLInputElement | null>(null)
 
     // storage a history of created elements
     const canvasObjects = useStorage((root) => root.canvasObjects)
@@ -63,6 +69,14 @@ export default function Page() {
         return canvasObjects.size === 0;
     }, []);
 
+    // -------
+    const deleteShapeFromStorage = useMutation(({storage}, objectId) => {
+        const canvasObjects = storage.get("canvasObjects");
+
+        canvasObjects.delete(objectId)
+    }, [])
+
+    // -------
     const handleActiveElement = (element: ActiveElement) => {
         setActiveElement(element)
 
@@ -72,13 +86,35 @@ export default function Page() {
                 fabricRef.current?.clear()
                 setActiveElement(defaultNavElement)
             break;
+            case 'delete':
+                handleDelete(fabricRef.current as any, deleteShapeFromStorage)
+                setActiveElement(defaultNavElement)
+                break;
+            // upload an image to the canvas
+            case "image":
+                // trigger the click event on the input element which opens the file dialog
+                imageInputRef.current?.click();
+                /**
+                 * set drawing mode to false
+                 * If the user is drawing on the canvas, we want to stop the
+                 * drawing mode when clicked on the image item from the dropdown.
+                 */
+                isDrawing.current = false;
 
+                if (fabricRef.current) {
+                    // disable the drawing mode of canvas
+                    if ("isDrawingMode" in fabricRef.current) {
+                        fabricRef.current.isDrawingMode = false;
+                    }
+                }
+                break;
             default:
                 break;
         }
 
         selectedShapeRef.current = element?.value as string
     }
+
 
     useEffect(() => {
         const canvas = initializeFabric({
@@ -102,23 +138,39 @@ export default function Page() {
             handleCanvaseMouseMove({
                 options,
                 canvas,
+                isDrawing,
                 selectedShapeRef,
+                shapeRef,
+                syncShapeInStorage,
+            });
+        });
+
+
+        // --------------------------------------
+        canvas.on("mouse:up", () => {
+            handleCanvasMouseUp({
+                canvas,
                 isDrawing,
                 shapeRef,
-                syncShapeInStorage
+                activeObjectRef,
+                selectedShapeRef,
+                syncShapeInStorage,
+                setActiveElement,
             });
         });
 
         // --------------------------------------
-        canvas.on("mouse:up", (options) => {
-            handleCanvasMouseUp({
-                canvas,
-                selectedShapeRef,
-                isDrawing,
-                shapeRef,
+        canvas.on("path:created", (options) => {
+            handlePathCreated({
+                options,
                 syncShapeInStorage,
-                setActiveElement,
-                activeObjectRef
+            });
+        });
+
+        // --------------------------------------
+        canvas?.on("object:moving", (options) => {
+            handleCanvasObjectMoving({
+                options,
             });
         });
 
@@ -126,31 +178,63 @@ export default function Page() {
         canvas.on("object:modified", (options) => {
             handleCanvasObjectModified({
                 options,
-                syncShapeInStorage
-            })
-        })
+                syncShapeInStorage,
+            });
+        });
+
 
         window.addEventListener("resize", () => {
             handleResize({
                 canvas: fabricRef.current,
             });
         });
+
+        window.addEventListener("keydown", (e) =>
+            handleKeyDown({
+                e,
+                canvas: fabricRef.current,
+                undo,
+                redo,
+                syncShapeInStorage,
+                deleteShapeFromStorage,
+            })
+        );
+
+        return () => {
+            canvas.dispose()
+        }
         }, [])
 
 
     useEffect(() => {
         renderCanvas({
-            fabricRef, canvasObjects, activeObjectRef
-        })
-    }, [canvasObjects])
-
+            fabricRef,
+            canvasObjects,
+            activeObjectRef,
+        });
+    }, [canvasObjects]);
 
     return (
         <main className="h-screen overflow-hidden">
-            <Navbar activeElement={activeElement} handleActiveElement={handleActiveElement}/>
+            <Navbar
+                imageInputRef={imageInputRef}
+                activeElement={activeElement}
+                handleImageUpload={(e: any) => {
+                    // prevent the default behavior of the input element
+                    e.stopPropagation();
+
+                    handleImageUpload({
+                        file: e.target.files[0],
+                        canvas: fabricRef as any,
+                        shapeRef,
+                        syncShapeInStorage,
+                    });
+                }}
+                handleActiveElement={handleActiveElement}
+            />
 
             <section className="flex h-full flex-row">
-                <LeftSidebar  />
+                <LeftSidebar allShapes={Array.from(canvasObjects)}  />
                 <Live canvasRef={canvasRef}/>
                 <RightSidebar />
             </section>
